8c4219c7a04f04b2b366cce2ac4a06ee278be527
ShenJieSuzhou <745689122@qq.com>
2017-10-25 11:26:03 +0800

    清理测试代码

--- CoolFrame/ViewControllers/SecondViewController.h
+++ CoolFrame/ViewControllers/SecondViewController.h
@@ -7,23 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "DownloadUtil.h"
 
-@interface SecondViewController : UIViewController<DownloadDelegate>
+@interface SecondViewController : UIViewController
 
 @property (nonatomic, strong) UILabel *label;
 
-@property (nonatomic, strong) UIView *downloadView;
-
-@property (nonatomic, strong) UIButton *cancelBtn;
-
-@property (nonatomic, strong) UIButton *controlBtn;
-
-@property (nonatomic, strong) UIImageView *imageV;
-
-@property (nonatomic, strong) UIProgressView *processView;
-
-@property (nonatomic, strong) UILabel *progressLabel;
-
-@property (assign) BOOL flag;
 @end
